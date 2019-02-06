29ef64055ce766449c0aca906728ff4dbf4a8f44
nacker <nacker@live.com>
2019-02-07 01:47:58 +0800

    init

---
+++ config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = {
+    debug: true,
+    secret:'blog',
+    pageSize:5,
+    uploadDir: path.join(__dirname,'uploads'),
+    avatarDir: path.join(__dirname,'uploads/avatar')
+};
---
+++ models/comment.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var db = require('./db');
+
+function Comment(comment) {
+    this.title = article.title;
+    this.content = comment.content;
+    this.time = comment.time;
+    this.uid = comment.uid;
+    this.aid = comment.aid;
+}
